0ce5606ade800a064ac888a7c5eb7efe4c5a79bc
esfox <osairis100@gmail.com>
2023-04-30 09:05:18 +0800

    Update api service

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,12 +1,16 @@
 import { PageContentDataType, PageType } from '@/types';
 
-const fetchApi = (route: string, options?: RequestInit) =>
-  fetch(`/api/${route.startsWith('/') ? route.slice(1) : route}`, options);
+import ky from 'ky';
+
+const api = ky.create({
+  prefixUrl: '/api',
+});
 
 export const apiService = {
   // TODO: Implement pagination
   async getPages() {
-    const { results } = await fetchApi('/pages').then((response) => response.json());
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { results } = await api.get('pages').json<any>();
 
     /* Map the notion pages to an instance of `PageType` */
     const pagesResult: PageType[] = [];
@@ -23,10 +27,11 @@ export const apiService = {
   },
 
   async getPageContent(pageId: string) {
-    const queryParams = new URLSearchParams({ withPreview: 'true' });
-    const data: PageContentDataType = await fetchApi(
-      `/pages/${pageId}/content?${queryParams}`
-    ).then((response) => response.json());
+    const data: PageContentDataType = await api
+      .get(`pages/${pageId}/content`, {
+        searchParams: new URLSearchParams({ withPreview: 'true' }),
+      })
+      .json();
     return data;
   },
 };
