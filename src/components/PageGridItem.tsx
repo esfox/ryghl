65f47e76a622074c9d454f16942e0d52b7ad26d6
esfox <osairis100@gmail.com>
2023-05-06 03:04:14 +0800

    Update page grid item linking

--- src/components/PageGridItem.tsx
+++ src/components/PageGridItem.tsx
@@ -14,20 +14,22 @@ export const PageGridItem: React.FC<PageGridItemProps> = ({ page, preview }) => 
   const { title } = page;
   return (
     <div className="grid place-items-center">
-      <div
-        className="w-[10rem] h-[15rem] shadow-md bg-white overflow-hidden cursor-pointer hover:shadow-xl hover:scale-105 transition-all"
-        role="button"
-      >
-        {preview ? (
-          <Image src={preview} alt="preview" width={160} height={240} className="p-4" />
-        ) : (
-          <div className="p-6">
-            <SkeletonTheme baseColor="#eaeaea" highlightColor="#eaeaea">
-              <Skeleton count={7} containerClassName="w-full" />
-            </SkeletonTheme>
-          </div>
-        )}
-      </div>
+      <Link href={`/pages/${page.id}`} className="mt-5">
+        <div
+          className="w-[10rem] h-[15rem] shadow-md bg-white overflow-hidden cursor-pointer hover:shadow-xl hover:scale-105 transition-all"
+          role="button"
+        >
+          {preview ? (
+            <Image src={preview} alt="preview" width={160} height={240} className="p-4" />
+          ) : (
+            <div className="p-6">
+              <SkeletonTheme baseColor="#eaeaea" highlightColor="#eaeaea">
+                <Skeleton count={7} containerClassName="w-full" />
+              </SkeletonTheme>
+            </div>
+          )}
+        </div>
+      </Link>
       <Link href={`/pages/${page.id}`} className="mt-5">
         {title}
       </Link>
